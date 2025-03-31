79fdacfa2a00ef4d74dca0f6192fa0db0a30234b
zakhalloran <157628041+zakhalloran@users.noreply.github.com>
2025-03-31 18:37:06 -0400

    Add files via upload
    March 31st Class, Functions.js functions.html

---
+++ functions.js
@@ -0,0 +1,7 @@
+function greetUser() {
+ let name = document.
+ getElementById("nameInput").value
+ ;
+ let message = "Hello, " + name + " ! Welcome to JavaScript.";
+ document.getElementById("greeting").innerHTML = message;
+}
